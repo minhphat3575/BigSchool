d696720a297a64a0ea003f9e8b6b32e134fb13fd
ASUS <minhphat3575@gmail.com>
2022-06-17 16:27:18 +0700

    update 17/06/2022

---
+++ BigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,41 @@
+﻿using BigSchool.DTOs;
+using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Exchange.WebServices.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BigSchool.Controllers
+{
+    
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend (AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeID == userId && a.CourseID == attendanceDto.CourseId))
+                return BadRequest("The Attendance Already exists!");
+            var attendance = new Attendance
+            {
+                CourseID = attendanceDto.CourseId,
+                AttendeeID = userId
+            };
+
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ BigSchool/DTOs/FollowingDto.cs
@@ -0,0 +1,14 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.DTOs
+{
+    public class FollowingDto
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public string FolloweeId { get; set; }
+    }
+}
